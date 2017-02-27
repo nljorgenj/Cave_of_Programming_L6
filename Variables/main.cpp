3405e02dbd12acb8998add192ba798f4a584dd7c
nljor <nljor@JONSSONS>
2017-02-27 23:34:40 +0100

    Add project files.

---
+++ Variables/main.cpp
@@ -0,0 +1,23 @@
+//*******************************************************
+// Name			: Variables.cpp
+// Author		: Jörgen Jönsson
+// Version		:
+// Copyright	: Your copyright notice
+// Description	: Hellow World in C++, Ansi-style
+//*******************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+
+
+
+
+
+	return 0;
+}
+
+
