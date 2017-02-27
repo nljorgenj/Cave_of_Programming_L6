a13c68176366e3ecd13a328cf9d8c73a38af1416
nljorgenj <nljorgenj@gmail.com>
2017-02-27 23:48:05 +0100

    Cave of programming Lecture 6

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -13,9 +13,21 @@ using namespace std;
 int main() {
 	
 
+	int numberCats = 5;
+	int numberDogs = 7;
+	int numberAnimals = numberCats + numberDogs;
 
 
+	cout << "Number of cats: " << numberCats << endl;
+	cout << "Number of dogs: " << numberDogs << endl;
 
+	cout << "Total number of animals: " << numberAnimals << endl;
+
+	cout << "New dog acquired!" << endl;
+
+	numberDogs = numberDogs + 1;
+
+	cout << "New number of dogs: " << numberDogs << endl;
 
 	return 0;
 }
